16d44325ecda5e801dfffc01f619fb64237ed31c
Rohan27s <rohangotwal3@gmail.com>
2024-12-12 11:58:37 +0530

    feat/GSAP added on Hero Banner

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,8 @@
 import React, { useRef, useState } from 'react'
 import Button from './Button'
 import { TiLocationArrow } from 'react-icons/ti'
-
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap';
 const Hero = () => {
     const [currentIndex, setcurrentIndex] = useState(1)
     const [hasClicked, setHasClicked] = useState(false)
@@ -17,6 +18,27 @@ const Hero = () => {
         setHasClicked(true);
         setcurrentIndex(upcomingVideoIndex)
     }
+    useGSAP(() => {
+        if (hasClicked) {
+            gsap.set('#next-video', { visibility: 'visible' })
+            gsap.to('#next-video', {
+                transformOrigin: 'center center',
+                scale: 1,
+                width: '100%',
+                height: '100%',
+                duration: 1,
+                ease: 'power1.inOut',
+                onStart: () => nextVideoRef.current.play()
+            })
+            gsap.from('#current-video', {
+                transformOrigin: 'center center',
+                scale: 0,
+                duration: 1.5,
+                ease: 'power1.inOut'
+            })
+        }
+    }, { dependencies: [currentIndex], revertOnUpdate: true })
+
     const getVideoSrc = (idx) => `videos/hero-${idx}.mp4`
     return (
         <div className='relative h-dvh w-screen overflow-x-hidden'>
