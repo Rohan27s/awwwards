6770e28427b513cc7044ccbecc2818e88dd30948
Rohan27s <rohan@Rohans-MacBook-Air.local>
2024-12-17 12:40:38 +0530

    feat/story added

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import About from "./components/About";
 import Features from "./components/Features";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
-
+import Story from "./components/Story";
 
 function App() {
   return (
@@ -11,8 +11,9 @@ function App() {
       <Hero />
       <About />
       <Features />
+      <Story />
     </main>
-  );
+  )
 }
 
 export default App;
